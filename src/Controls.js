523a51e4aab6fb91d4024150651991ff0bc7de94
patrick hrabos <phrabos@gmail.com>
2021-06-01 11:06:13 -0400

    finish app logic and refactor radio button controls

--- src/Controls.js
+++ src/Controls.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-const Controls = ({ choice, computer, handleSubmit }) => {
+const Controls = ({ choiceSpan, computerSpan, outcome, handleRadio, handleSubmit, handleReset }) => {
 
   return (
     <div>
-    <form value={choice} onChange={handleSubmit}>
-    <input type='radio' value='rock' name='choice' /><span>Rock</span>
-    <input type='radio' value='paper' name='choice' /><span>Paper</span>
-    <input type='radio' value='scissors' name='choice' /><span>Scissor</span>
+    <form onSubmit={handleSubmit}>
+    <label>
+      <input onChange={handleRadio} type='radio' value='rock' name='choice' />Rock
+    </label>
+    <label>
+      <input onChange={handleRadio} type='radio' value='paper' name='choice' />Paper
+    </label>
+    <label>
+      <input onChange={handleRadio} type='radio' value='scissor' name='choice' />Scissor
+    </label>
     <button type='submit'>Throw</button>
     </form>
-    <p>You picked {choice}, computer picked {computer}</p>
-    <p>Outcome: You</p>
+    <p>You picked {choiceSpan}, computer picked {computerSpan}</p>
+    <p>Outcome: You {outcome}</p>
+    <button onClick={handleReset}>reset</button>
     </div>
   )
 }
--- src/DisplayContainer.js
+++ src/DisplayContainer.js
@@ -1,22 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Controls from './Controls';
 
 const DisplayContainer = () => {
-  const [choice, setChoice] = useState('rock');
+  const [choice, setChoice] = useState('');
+  const [choiceSpan, setChoiceSpan] = useState('');
   const [computer, setComputer] = useState('');
+  const [computerSpan, setComputerSpan] = useState('');
+  const [outcome, setOutcome] = useState('');
+
+  useEffect(() => {
+    if(computer === 0) setComputerSpan('rock')
+    if(computer === 1) setComputerSpan('paper')
+    if(computer === 2) setComputerSpan('scissor')
+
+    if(computer === 0 && choice === 'rock') setOutcome('draw')
+    else if(computer === 0 && choice === 'paper') setOutcome('win')
+    else if(computer === 0 && choice === 'scissors') setOutcome('lose')
+    else if(computer === 1 && choice === 'rock') setOutcome('lose')
+    else if(computer === 1 && choice === 'paper') setOutcome('draw')
+    else if(computer === 1 && choice === 'scissor') setOutcome('win')
+    else if(computer === 2 && choice === 'rock') setOutcome('win')
+    else if(computer === 2 && choice === 'paper') setOutcome('lose')
+    else if(computer === 2 && choice === 'scissor') setOutcome('draw')
+  }, [computer])
+
+  const handleRadio = (e) => {
+    setChoice(e.target.value);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setChoice(e.target.value);
+    console.log('button', choice);
+    setChoiceSpan(choice);
     setComputer(Math.floor(Math.random() * 3))
   }
 
-
+  const handleReset = () => {
+    setOutcome('');
+    setComputerSpan('');
+    setChoiceSpan('');
+  }
 
   return(
     <>
     <h1>Rock Paper Scissor</h1>
-    <Controls choice={choice} computer={computer} handleSubmit={handleSubmit} />
+    <Controls 
+      choiceSpan={choiceSpan}
+      computerSpan={computerSpan}
+      computer={computer} 
+      handleRadio={handleRadio}
+      handleSubmit={handleSubmit}
+      outcome={outcome}
+      handleReset={handleReset} />
     </>
   ) 
 }
